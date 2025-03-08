64655d11232be439579c0f240a7cba3995163c32
DraouiRihab <draoui.rihab3@gmail.com>
2025-03-08 11:25:59 +0100

    my first commit

---
+++ src/function.js
@@ -0,0 +1,12 @@
+export function add_number(n1,n2){
+    return n1+n2
+}
+export function multiply_nomber(n1,n2){
+    return n1*n2
+}
+
+/* commenjs
+module.exports={
+ add:add_number,
+ multiply:multiply_nomber
+}*/
